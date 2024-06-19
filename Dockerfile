3ecd7c00688b4c308efc7ad1266987f1a13f2f42
Edward Bunt <44599+ebunt@users.noreply.github.com>
2024-06-19 15:45:13 -0400

    testing env vars and connections

--- Dockerfile
+++ Dockerfile
@@ -1,1 +1,7 @@
 FROM quay.io/astronomer/astro-runtime:11.5.0
+
+# connections
+ENV AIRFLOW_CONN_AWS_DEFAULT='{"conn_type":"aws"}'
+
+# variables
+ENV AIRFLOW_VAR_SNS_ARN='arn:aws:sns:us-east-1:721947922831:airflow-sns-notifications'
--- dags/notifications.py
+++ dags/notifications.py
@@ -2,18 +2,26 @@ from datetime import datetime
 from airflow import DAG
 from airflow.operators.bash import BashOperator
 
-from include.callbacks.aws import dag_failure_sns_notification, task_failure_sns_notification
+from include.callbacks.aws import (
+    dag_failure_sns_notification,
+    # task_failure_sns_notification,
+)
 
 
 with DAG(
     dag_id="sns_notifications",
-    schedule="@once",
+    schedule=None,
     start_date=datetime(2023, 1, 1),
     on_failure_callback=[dag_failure_sns_notification],
     catchup=False,
 ):
-    BashOperator(
+    print_env = BashOperator(
+        task_id="print_env", bash_command="printenv AIRFLOW__CORE__DAG_CONCURRENCY"
+    )
+    fail_task = BashOperator(
         task_id="failing_task",
-        on_failure_callback=[task_failure_sns_notification],
+        # on_failure_callback=[task_failure_sns_notification],
         bash_command="fail",
     )
+
+    print_env >> fail_task
